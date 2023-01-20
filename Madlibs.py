26ebaa0cc4545c128f67459e982f204e67cb92d6
Milena Polini <milenapolini@MacBook-Air-de-Milena.local>
2023-01-20 14:54:48 -0300

    first commit

---
+++ Madlibs.py
@@ -0,0 +1,24 @@
+#MADLIBS
+
+# 1. String concatenation:
+
+
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+animal = input("Animal: ")
+place = input("Place: ")
+
+madlib = f"I'm {noun1}! Who are you? \
+Are you {noun2}, too? \
+Then there's a pair of us -- don't {verb1}! \
+They'd {verb2} -- you know! \
+How dreary to be {noun3}! \
+How public like a {animal} \
+To {verb3} one's name the livelong day \
+To an admiring {place}!"
+
+print(madlib)
